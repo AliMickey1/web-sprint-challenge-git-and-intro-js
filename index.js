b21b47b289970133bcaf438842d741ff79e3b59f
AliMickey1 <alicesonphillips@gmail.com>
2022-10-01 11:21:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -268,9 +268,16 @@ Use removeArtist to do the following:
 5. Return the resulting copied array
 🌟 EXAMPLE: if removeArtist is invoked with the artists array and the number 0, it will return the resulting array with Amedeo Modigliani removed from our dataset. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
-}
+function removeArtist(array,number) {
+  const takeAway = [...array];
+
+  takeAway.splice(number,1);
+    console.log(takeAway);
+  return takeAway;
+
+  }
+
+removeArtist(artists, 0);
 
 
 
@@ -290,11 +297,23 @@ Use addArtist to do the following:
 5. Add the newly created object to the copied array, then return the copied array
 🌟 EXAMPLE: Invoking addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.') should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array, inpName, inpYears, inpGenre, inpNationality, inpBio) {
+  
+  let newGuy = [];
+  newGuy = [...array]
+ 
+  newGuy.push({
+   name: inpName,
+   years: inpYears,
+   genre: inpGenre,
+   nationality: inpNationality,
+   bio: inpBio,
+  });
+  return newGuy;
+ console.log(newGuy);
 }
 
-
+console.log(addArtist(artists, 'John Doe', '1988-2022', 'Full Stack Development', 'African American', 'I have a background in customer service at Big Retail Chain. I am attending BloomTech to become a Frontend Developer.'));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use lotsOfArt to do the following: 
@@ -304,10 +323,19 @@ Use lotsOfArt to do the following:
 🌟 EXAMPLE: lotsOfArt(artists) will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]
 */
 
-function lotsOfArt(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lotsOfArt(array) {
+const newArray = [];
+  for(let i = 0; i < array.length; i++)
+    {
+      if(array[i].paintings > 100)
+        {
+          newArray.push(array[i].name);
+        }
+    }
+    return newArray;
 }
 
+lotsOfArt(artists);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 8: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -320,10 +348,22 @@ Use artistInfo to do the following:
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
 */
 
-function artistInfo(/*Your Code Here*/){
-  /*Your Code Here*/
-}
+function artistInfo(array, name)
+{
+  const artistBio = [];
+  for(let i = 0; i < array.length; i++)
+    {
 
+          if(array[i].name === name)  
+          {
+            let myArtist = array[i].bio;
+            artistBio.push(myArtist);
+          }
+    }
+  return artistBio;
+
+}
+artistInfo(artists, 'Frida Kahlo');
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 9: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -336,9 +376,23 @@ Use artistByCountry to do the following:
 🌟 EXAMPLE: Invoking artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(/*Your Code Here*/){
-  /*Your Code Here*/
+function artistByCountry(array,national)
+{
+
+const byCountry = [];
+  for(let i = 0; i < array.length; i++)
+    {
+          if(array[i].nationality === national)  
+          {
+            let trueArtist = array[i].name;
+            byCountry.push(trueArtist);
+            }
+    }
+  return byCountry;
+  console.log(byCountry)
 }
+
+artistByCountry(artists, 'Spanish');
 
 
 
